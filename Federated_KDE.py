ba42aef012a3231f68edaa806d1163d511a41b3e
Michael S. <mspitz2005@gmail.com>
2025-04-28 12:55:18 -0400

    Initial Commit.

---
+++ Federated_KDE.py
@@ -0,0 +1,25 @@
+import pandas as pd
+class User:
+    def __init__(self, d):
+        self.D = pd.Series() # What exactly would the user's location vector look like?
+
+    # Evaluates G_h(d_i)
+    def evaluate(self, d_i):
+
+    def gaussianKernel(self, ):
+    # Return evaluations to the server
+    def updateServer(self, destination, ):
+        Server(destination)
+
+class Server:
+    def __init__(self, corner1, corner2, h):
+        self.h = h
+
+d1, d2, di_1, di = None
+N = [User(d1), User(d2), ..., User(di_1), User(di)] # Number of Users
+
+
+PxQ =
+
+for user in N:
+    user.evaluate()
---
+++ script_utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def gaussian_kernel(h, x, y):
+    delta_inner = np.power((x - y), 2)
+    delta = np.abs(delta_inner)
+    euler_inner = -1 * (np.power(delta, 2)) / (2 * h * h)
+    np.exp(euler_inner)
+
+# Computes the gaussian KDE across set given point
+def gaussian_kde(d_i, dataset):
+
+    for row in dataset.rows:
+        for column in dataset.columns:
+            gaussian_kernel(d_i)
+
+
+
+# Derivation of the Gaussian Kernel
+def random_fourier_features(x, y):
+    gaussian_kernel(h, x, y)
